1858b356802b5f122ce633042eb86a7d486cd449
Firmshrimp <94081909+Firmshrimp@users.noreply.github.com>
2021-11-10 21:23:59 -0600

    Initial commit

---
+++ src/_data/site.js
@@ -0,0 +1,26 @@
+module.exports = {
+    layout: 'layouts/default.njk',
+    url: 'https://www.example.com',
+    name: '11ty Template',
+    subtitle: 'A sample site using Eleventy',
+    author: {
+        fullName: 'Author',
+        biography:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum augue turpis, ornare sed tortor ut, lacinia aliquam nisl. Phasellus tellus turpis, fermentum vitae commodo ac, euismod sed magna. Nunc laoreet elementum tellus, eget commodo nisi suscipit nec.'
+    },
+    primaryNav: [
+        { title: 'Uses', subtitle: 'How I Work', url: '/uses' },
+        { title: 'Now', subtitle: "What I'm Doing", url: '/now' }
+    ],
+    socialMedia: [
+        { title: 'Facebook', subtitle: '', url: '//www.facebook.com' },
+        { title: 'Instagram', subtitle: '', url: '//www.instagram.com' },
+        { title: 'Twitter', subtitle: '', url: '//www.twitter.com' },
+        { title: 'LinkedIn', subtitle: '', url: '//www.linkedin.com' },
+        { title: 'Twitch', subtitle: '', url: '//www.twitch.tv' },
+        { title: 'YouTube', subtitle: '', url: '//www.youtube.com' }
+    ],
+    copyright: new Date(),
+    lastMod: new Date(),
+    changeFreq: 'daily'
+};
